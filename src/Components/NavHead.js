def970bc33132e99052e420fc2058253640a6565
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-02 12:43:57 +0500

    yo

--- src/Components/NavHead.js
+++ src/Components/NavHead.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import "./NavHead.css"; // Import the CSS file
-
+import "./NavHead.css";
 function NavHead() {
   const [navbarBg, setNavbarBg] = useState("bg-transparent");
   const [isNavOpen, setNavOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -29,64 +29,65 @@ function NavHead() {
   const toggleNav = () => {
     setNavOpen(!isNavOpen);
   };
-
   return (
-    <nav className={`navbar navbar-expand-lg ${navbarBg} fixed-top`}>
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img src="/berserk-gym.png" alt="gym logo" className="img-fluid" />
-        </a>
-        <button
-          className={`navbar-toggler ${isNavOpen ? "open" : ""}`}
-          type="button"
-          onClick={toggleNav}
-        >
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </button>
-        <div
-          className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`}
-          id="navbarSupportedContent"
-        >
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link why-us" href="#">
-                Why us?
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Services
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Gallery
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Contact Us
-              </a>
-            </li>
-          </ul>
-          <a href="#" className="btn-join-now">
-            Join Now <i className="fas fa-arrow-right"></i>
+    <>
+      <nav className={`navbar navbar-expand-lg ${navbarBg} fixed-top`}>
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="/berserk-gym.png" alt="gym logo" className="img-fluid" />
           </a>
+          <button
+            className={`navbar-toggler ${isNavOpen ? "open" : ""}`}
+            type="button"
+            onClick={toggleNav}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </button>
+          <div
+            className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`}
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link why-us" href="#">
+                  Why us?
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Gallery
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+            <button className="btn-join-now">
+              Join Now <i className="fas fa-arrow-right"></i>
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
