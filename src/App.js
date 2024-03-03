e60d3339e6c4079cbc7b64ad9bfee7fbe79af3b9
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-03 10:29:26 +0500

    hero text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroSection from "./Components/Hero-Section/HeroSection.js";
 import NavHead from "./Components/Navbar/NavHead.js";
 
 function App() {
   return (
     <div className="app">
       <NavHead />
+      <HeroSection />
     </div>
   );
 }
--- src/Components/Hero-Section/HeroSection.js
+++ src/Components/Hero-Section/HeroSection.js
@@ -1,9 +1,32 @@
 import React from "react";
+import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
-      <div className="left">left</div>
+      <div className="left">
+        <div className="hero-text">
+          <div className="top-text">
+            <span>BUILD YOUR</span>
+          </div>
+          <div className="bottom-text">
+            <span className="stroke-text">DREAM </span>
+            <span>PHYSIQUE</span>
+          </div>
+          <div className="description">
+            <div>
+              Discover the path to your ideal shape & strength with our
+              transformative
+            </div>
+            <div> fitness programs & modern equipments.</div>
+          </div>
+        </div>
+        <div className="cta-buttons">
+          <button className="solid-button">Free Trial Pass</button>
+          <button className="outline-button">Explore Services</button>
+        </div>
+      </div>
+
       <div className="right">right</div>
     </div>
   );
