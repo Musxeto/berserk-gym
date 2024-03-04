340155faf071397eac3211c5922609ba500381b9
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-04 13:24:26 +0500

    why us gallery

--- src/Components/WhyUs/WhyUsGallery.js
+++ src/Components/WhyUs/WhyUsGallery.js
@@ -1,45 +1,39 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
+import "./WhyUsGallery.css"; // Import the custom CSS file
 
 const WhyUsGallery = () => {
   return (
-    <Row className="gallery">
-      <Col lg={4} md={6} sm={12} className="mb-3">
-        <img
-          className="img-fluid"
-          src="https://placekitten.com/800/400" // Replace with your image URL
-          alt="Gallery Image 1"
-        />
-      </Col>
-      <Col lg={4} md={6} sm={12} className="mb-3">
-        <img
-          className="img-fluid"
-          src="https://placekitten.com/800/401" // Replace with your image URL
-          alt="Gallery Image 2"
-        />
-      </Col>
-      <Col lg={4} md={6} sm={12} className="mb-3">
-        <img
-          className="img-fluid"
-          src="https://placekitten.com/800/402" // Replace with your image URL
-          alt="Gallery Image 3"
-        />
-      </Col>
-      <Col lg={4} md={6} sm={12} className="mb-3">
-        <img
-          className="img-fluid"
-          src="https://placekitten.com/800/403" // Replace with your image URL
-          alt="Gallery Image 4"
-        />
-      </Col>
-      <Col lg={4} md={6} sm={12} className="mb-3">
-        <img
-          className="img-fluid"
-          src="https://placekitten.com/800/404" // Replace with your image URL
-          alt="Gallery Image 5"
-        />
-      </Col>
-    </Row>
+    <Container>
+      <Row>
+        {/* First Picture - Covers Complete Row */}
+        <Col xs={12}>
+          <img
+            src="https://png.pngtree.com/background/20230516/original/pngtree-gym-with-many-weights-in-dark-lighting-picture-image_2611113.jpg"
+            alt="First Image"
+            className="img-fluid custom-img"
+          />
+        </Col>
+      </Row>
+      <Row>
+        {/* Second Picture - Half Row */}
+        <Col xs={6}>
+          <img
+            src="https://img.freepik.com/free-photo/low-angle-view-unrecognizable-muscular-build-man-preparing-lifting-barbell-health-club_637285-2497.jpg"
+            alt="Second Image"
+            className="img-fluid custom-img"
+          />
+        </Col>
+        {/* Third Picture - Half Row */}
+        <Col xs={6}>
+          <img
+            src="https://img.freepik.com/photos-premium/photo-jeune-homme-s-entrainant-dans-salle-sport_763111-16971.jpg"
+            alt="Third Image"
+            className="img-fluid custom-img"
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
