a78dac7ea8a1bd0482f6fe84672decffe95f5b2a
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-04 12:08:40 +0500

    dumbell li

--- src/Components/WhyUs/WhyUsReasons.js
+++ src/Components/WhyUs/WhyUsReasons.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { FaDumbbell } from "react-icons/fa";
+import "./WhyUs.css"; // Assuming you have a CSS file for additional styling
 
 const WhyUsReasons = () => {
-  return <div>reasons</div>;
+  return (
+    <div>
+      <h3>Reasons to Choose Berserk Gym</h3>
+      <ul className="reasons-list">
+        <li>
+          <FaDumbbell /> State-of-the-art fitness equipment.
+        </li>
+        <li>
+          <FaDumbbell /> Expert trainers for personalized guidance.
+        </li>
+        <li>
+          <FaDumbbell /> Diverse and engaging workout programs.
+        </li>
+        <li>
+          <FaDumbbell /> Friendly and motivating community.
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default WhyUsReasons;
