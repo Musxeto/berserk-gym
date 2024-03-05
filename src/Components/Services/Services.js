24643b31c2c883704558476439ded072920f197d
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-05 10:23:55 +0500

    card css

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -2,14 +2,43 @@ import React from "react";
 import "./Services.css";
 const Services = () => {
   return (
-    <div className="services">
-      <div>EXPLORE OUR SERVICES</div>
-      <div>FOR A BETTER AND HEALTHY LIFESTYLE</div>
+    <div className="services text-center">
+      <div className="heading-big">EXPLORE OUR SERVICES</div>
+      <div className="heading-small">FOR A BETTER AND HEALTHY LIFESTYLE</div>
       <div className="container-fluid">
-        <div className="row no-gutters">
-          <div className="card col-lg-3"></div>
+        <div className="row no-gutters justify-content-around ">
+          <div
+            id="strength-training"
+            className="card-custom col-lg-3 col-md-6 col-sm-12"
+          >
+            <div className="card-icon"></div>
+            <div className="card-heading"></div>
+            <div className="card-description"></div>
+          </div>
+          <div id="cardio" className="card-custom col-lg-3 col-md-6 col-sm-12">
+            <div className="card-icon"></div>
+            <div className="card-heading"></div>
+            <div className="card-description"></div>
+          </div>
+          <div
+            id="crossfit"
+            className="card-custom col-lg-3 col-md-6 col-sm-12"
+          >
+            <div className="card-icon"></div>
+            <div className="card-heading"></div>
+            <div className="card-description"></div>
+          </div>
+          <div
+            id="fat-loss"
+            className="card-custom col-lg-3 col-md-6 col-sm-12"
+          >
+            <div className="card-icon"></div>
+            <div className="card-heading"></div>
+            <div className="card-description"></div>
+          </div>
         </div>
       </div>
+      niggas
     </div>
   );
 };
