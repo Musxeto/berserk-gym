f17dbcfde85bc5d0fa17722d08bbee7e934154d6
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-05 09:26:09 +0500

    chris paul

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Counts from "./Components/Counts/Counts.js";
 import HeroSection from "./Components/Hero-Section/HeroSection.js";
 import NavHead from "./Components/Navbar/NavHead.js";
+import Services from "./Components/Services/Services.js";
 import WhyUs from "./Components/WhyUs/WhyUs.js";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <HeroSection />
       <Counts />
       <WhyUs />
+      <Services />
     </div>
   );
 }
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Services.css";
+const Services = () => {
+  return (
+    <div className="services">
+      <div>EXPLORE OUR SERVICES</div>
+      <div>FOR A BETTER AND HEALTHY LIFESTYLE</div>
+      <div className="container-fluid">
+        <div className="row no-gutters">
+          <div className="card col-lg-3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
