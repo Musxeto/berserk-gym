3dc5b29f082575b9a3abec3b2185f0edf4546b0b
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-05 10:48:41 +0500

    kel tec gon make a nig

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,44 +1,98 @@
 import React from "react";
+import {
+  FaArrowRight,
+  FaDumbbell,
+  FaHeartbeat,
+  FaFire,
+  FaWeight,
+} from "react-icons/fa";
 import "./Services.css";
+
 const Services = () => {
   return (
     <div className="services text-center">
       <div className="heading-big">EXPLORE OUR SERVICES</div>
       <div className="heading-small">FOR A BETTER AND HEALTHY LIFESTYLE</div>
       <div className="container-fluid">
-        <div className="row no-gutters justify-content-around ">
+        <div className="row no-gutters justify-content-around">
+          {/* Strength Training */}
           <div
             id="strength-training"
-            className="card-custom col-lg-3 col-md-6 col-sm-12"
+            className="card-custom col-lg-3 col-md-6 col-sm-12 text-left" // Add text-left class
           >
-            <div className="card-icon"></div>
-            <div className="card-heading"></div>
-            <div className="card-description"></div>
+            <div className="card-icon">
+              <FaDumbbell />
+            </div>
+            <div className="card-heading">Strength Training</div>
+            <div className="card-description">
+              Build strength and endurance with our personalized strength
+              training programs. Our expert trainers will guide you through
+              effective workouts tailored to your fitness goals.
+            </div>
+            <div className="join-now">
+              Join Now <FaArrowRight />
+            </div>
           </div>
-          <div id="cardio" className="card-custom col-lg-3 col-md-6 col-sm-12">
-            <div className="card-icon"></div>
-            <div className="card-heading"></div>
-            <div className="card-description"></div>
+
+          {/* Cardio */}
+          <div
+            id="cardio"
+            className="card-custom col-lg-3 col-md-6 col-sm-12 text-left" // Add text-left class
+          >
+            <div className="card-icon">
+              <FaHeartbeat />
+            </div>
+            <div className="card-heading">Cardio</div>
+            <div className="card-description">
+              Boost your cardiovascular health with our dynamic cardio workouts.
+              Whether you prefer running, cycling, or high-intensity interval
+              training, we've got the perfect cardio exercises for you.
+            </div>
+            <div className="join-now">
+              Join Now <FaArrowRight />
+            </div>
           </div>
+
+          {/* Crossfit */}
           <div
             id="crossfit"
-            className="card-custom col-lg-3 col-md-6 col-sm-12"
+            className="card-custom col-lg-3 col-md-6 col-sm-12 text-left" // Add text-left class
           >
-            <div className="card-icon"></div>
-            <div className="card-heading"></div>
-            <div className="card-description"></div>
+            <div className="card-icon">
+              <FaFire />
+            </div>
+            <div className="card-heading">Crossfit</div>
+            <div className="card-description">
+              Experience the intensity of Crossfit training. Our Crossfit
+              programs combine strength, agility, and endurance exercises to
+              push your limits and achieve peak fitness.
+            </div>
+            <div className="join-now">
+              Join Now <FaArrowRight />
+            </div>
           </div>
+
+          {/* Fat Loss */}
           <div
             id="fat-loss"
-            className="card-custom col-lg-3 col-md-6 col-sm-12"
+            className="card-custom col-lg-3 col-md-6 col-sm-12 text-left" // Add text-left class
           >
-            <div className="card-icon"></div>
-            <div className="card-heading"></div>
-            <div className="card-description"></div>
+            <div className="card-icon">
+              <FaWeight />
+            </div>
+            <div className="card-heading">Fat Loss</div>
+            <div className="card-description">
+              Shed excess fat and achieve your weight loss goals with our
+              targeted fat loss programs. Our trainers will guide you through
+              effective workouts and provide nutrition advice for lasting
+              results.
+            </div>
+            <div className="join-now">
+              Join Now <FaArrowRight />
+            </div>
           </div>
         </div>
       </div>
-      niggas
     </div>
   );
 };
