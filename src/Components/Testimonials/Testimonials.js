e6243fdd2fbb7eba30c51ce6e1183ea9f6c61223
Mus <138971833+Musxeto@users.noreply.github.com>
2024-03-07 01:09:55 +0500

    real

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -11,36 +11,55 @@ import "swiper/css/pagination";
 
 const data = [
   {
-    img: "/pic1.png",
-    name: "Erenrst Wing",
+    img: "/lassi.jpg",
+    name: "Abdullah Lassi",
     review:
-      "amet consectetur adipisicing elit. Odit maiores, facilis corporis officiis adipisicing elit. Odit maiores, facilis corporis officiis.adipisicing elit. Odit ",
+      "I had an amazing experience at the gym. The trainers are highly skilled, and the atmosphere is motivating. I've seen incredible results in just a few weeks!",
   },
   {
-    img: "/pic1.png",
-    name: "Hilray Wing",
+    img: "/moiz.jpg",
+    name: "Moiz Mogger",
     review:
-      "amet consectetur adipisicing elit. Odit maiores, facilis corporis officiis.adipisicing elit. Odit maiores, facilis corporis officiis corporis officiis",
+      "The gym is fantastic! The variety of equipment and classes cater to all fitness levels. The staff is friendly, and the facilities are always clean. Highly recommended!",
   },
   {
-    img: "/pic1.png",
-    name: "Erenrst Double",
+    img: "/noel.jpg",
+    name: "Noel Deyzel",
     review:
-      "amet consectetur adipisicing elit. Odit maiores, facilis corporis officiis adipisicing elit. Odit maiores, facilis corporis officiis.adipisicing elit. Odit ",
+      "I joined the gym for its welcoming community, and I wasn't disappointed. The group classes are fun, and I've made great friends along my fitness journey.",
   },
   {
-    img: "/pic1.png",
-    name: "Dribnksd Rring",
+    img: "/mus.jpg",
+    name: "Mustafa (me)",
     review:
-      "amet consectetur adipisicing elit. Odit maiores,adipisicing elit. Odit maiores, facilis corporis officiis facilis corporis officiis corporis officiis",
+      "The trainers here are dedicated to helping you achieve your goals. They provide personalized workout plans and nutritional advice. It's a game-changer!",
+  },
+  {
+    img: "/michael.jpg",
+    name: "Michael Johnson",
+    review:
+      "I love the positive energy at this gym! The instructors are enthusiastic, and the diverse range of classes keeps things interesting. It's a great place to stay fit and have fun.",
+  },
+  {
+    img: "/braun.jpeg",
+    name: "Braun Anderson",
+    review:
+      "The gym's modern equipment and clean environment make it a top-notch fitness facility. The staff is always ready to assist, creating a comfortable workout experience.",
+  },
+  {
+    img: "/laid.jpg",
+    name: "David Laid",
+    review:
+      "This gym has everything I need for a complete workout. The atmosphere is motivating, and the trainers are knowledgeable. I'm impressed with my fitness progress!",
   },
 ];
 
 const Testimonials = () => {
   return (
     <section id="testimonial">
-      <h5>Review from clients</h5>
-      <h2>Testimonials</h2>
+      <div className="heading-big">TESTIMONIALS</div>
+      <div className="heading-small">REVIEW FROM CLIENTS</div>
+      <div className="blur blur-test"></div>
       <Swiper
         className="container testimonials__container"
         pagination={{ clickable: true }}
@@ -50,11 +69,12 @@ const Testimonials = () => {
           return (
             <SwiperSlide className="testimonial" key={index}>
               <div className="client__avatar">
-                <img src="/pic1.png" alt="avatar" />
+                <img src={dat.img} alt="avatar" />
               </div>
 
               <h5 className="clients__name">{dat.name}</h5>
               <small className="clients__review">{dat.review}</small>
+              <br />
             </SwiperSlide>
           );
         })}
